7f65d43442de4c59f5dcfb25e2c91f10ec423253
Rakesh <vrk4frnds@gmail.com>
2021-08-20 01:27:12 +0530

    added terraform file

--- instance.tf
+++ instance.tf
@@ -1,10 +1,6 @@
 locals {
   custom_data = <<CUSTOM_DATA
   #!/bin/bash
-  sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-  curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-  sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-  sudo apt-get update && sudo apt-get install terraform  
   CUSTOM_DATA
 }
 
---
+++ terraform.tfvars
@@ -0,0 +1,6 @@
+prefix         = "terraform"
+subscriptionID = "30ae1b96-baa4-459c-a7cb-39b2998c0227"
+location       = "eastus"
+username       = "trainer"
+password       = "Indian@1234567"
+# countofvms    = 2 
